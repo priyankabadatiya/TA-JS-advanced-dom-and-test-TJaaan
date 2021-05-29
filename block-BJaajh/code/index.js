426145127970f44efbbab258ea6a2143f15eb5bf
priyabadatiya <priyabadatiya@gmail.com>
2021-05-29 23:19:56 +0530

    assignment Test With Jest

--- block-BJaajh/code/index.js
+++ block-BJaajh/code/index.js
@@ -7,18 +7,17 @@ function checkFive(num) {
     } else {
         result = num + ' is greater than 5.';
     }
-
     return result;
 }
 
-function getWinner(guess1, guess2) {
-    if (guess1 === guess2) {
+function getWinner(guest1, guest2) {
+    if (guest1 === guest2) {
         return "TIE";
-    } else if (guess1 === "Paper" && guess2 === "Rock") {
+    } else if (guest1 === "Paper" && guest2 === "Rock") {
         return "Player 1 wins!";
-    } else if (guess1 === "Scissor" && guess2 === "Paper") {
+    } else if (guest1 === "Scissor" && guest2 === "Paper") {
         return "`Player 1 wins!";
-    } else if (guess1 === "Rock " && guess2 === "Scissors") {
+    } else if (guest1 === "Rock " && guest2 === "Scissors") {
         return "Player 1 wins";
     } else {
         return "Player 2 wins!";
