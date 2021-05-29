8a4a019f12a6feef17e22d7d488280b0c653bfee
priyabadatiya <priyabadatiya@gmail.com>
2021-05-29 23:22:31 +0530

    assignment Test

--- block-BJaajh/code/index.test.js
+++ block-BJaajh/code/index.test.js
@@ -12,18 +12,18 @@ test("7 is greater than 5 ", () => {
     expect(checkFive(7)).toBe("5 is equal to 5.");
 });
 
-test("if guest1 is rock and guests2 is paper then player1 wins", () => {
+test("if guest1 is rock and guest2 is paper then player1 wins", () => {
     expect(getWinner(Paper, Rock)).toBe("Player 1 wins!");
 });
 
-test("if guest1 is Paper and guests2 is Paper then player1 wins ", () => {
+test("if guest1 is Paper and guest2 is Paper then player1 wins ", () => {
     expect(getWinner(Scissor, Paper)).toBe("Player 1 wins!");
 });
 
-test("if guest1 is Paper and guests2 is Paper then player1 wins ", () => {
+test("if guest1 is Paper and guest2 is Paper then player1 wins ", () => {
     expect(getWinner(Scissor, Paper)).toBe("Player 1 wins!");
 });
 
-test("if guest1 is Rock and guests2 is Scissors then player1 wins ", () => {
+test("if guest1 is Rock and guest2 is Scissors then player1 wins ", () => {
     expect(getWinner(Rock, Scissors)).toBe("Player 1 wins!");
 });
