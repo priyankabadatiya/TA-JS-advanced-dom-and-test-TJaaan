057de770a2650cc2d93778b3cd615fe349ba652b
priyabadatiya <priyabadatiya@gmail.com>
2021-05-29 19:41:28 +0530

    assigment

--- block-BJaajc/code/add.test.js
+++ block-BJaajc/code/add.test.js
@@ -1,47 +1,47 @@
-const TestScheduler = require("@jest/core")
-const getFullName, isPalindrome, getCircumfrence, getArea = require("./index")
+const { TestScheduler } = require("@jest/core")
+const { getFullName, isPalindrome, getCircumfrence, getArea, } = require("./index")
 
-test("adds piku and piku to give piku piku", () => {
-    expect(getFullName("piku", "[piku]")).toBe("piku piku")
+test("adds piku and sonche to give piku sonche", () => {
+    expect(getFullName("piku", "sonche")).toBe("piku sonche")
 })
-test("adds piku and poku to give Joel Rajesh", () => {
-    expect(getFullName("piku", "poku")).toBe("piku poku")
+test("adds piku and sonche to give piku sonche", () => {
+    expect(getFullName("priyap", "Aarti")).toBe("priyap Aarti")
 })
-test("adds sugu and piku to give sugu piku", () => {
-    expect(getFullName("sugu", "piku")).toBe("sugu piku")
+test("adds piku and sonche to give piku sonche", () => {
+    expect(getFullName("Aarti", "guptas")).toBe("Aarti guptas")
 })
-test("adds shin and chan to give shin chan", () => {
-    expect(getFullName("shin", "chan")).not.toBe("shin chan")
+test("adds piku and sonche to give piku sonche", () => {
+    expect(getFullName("Aarti", "guptas")).not.toBe("piku sonche")
 })
-test("adds pikui and poko to give pikui poko", () => {
-    expect(getFullName("pikui", "poko")).not.toBe("pikui poko")
+test("adds piku and sonche to give piku sonche", () => {
+    expect(getFullName("piku", "sonche")).not.toBe("Aarti guptas")
 })
 
 
-test("adds pikui and pokoi to give pikui pokoi", () => {
+test("adds piku and sonche to give piku sonche", () => {
     expect(isPalindrome("mom")).toBe(true)
 })
-test("adds pikui and pokoi to give pikui pokoi", () => {
+test("adds piku and sonche to give piku sonche", () => {
     expect(isPalindrome("malayalam")).not.toBe(false)
 })
-test("adds pikui and pokoi to give pikui pokoi", () => {
+test("adds piku and sonche to give piku sonche", () => {
     expect(isPalindrome("malayalam")).toBe(true)
 })
-test("adds pikui and pokoi to give pikui pokoi", () => {
+test("adds piku and sonche to give piku sonche", () => {
     expect(isPalindrome("momm")).not.toBe(true)
 })
-test("adds pikui and pokoi to give pikui pokoi", () => {
+test("adds piku and sonche to give piku sonche", () => {
     expect(getCircumfrence("7")).toBe(`The circumference is 44`)
 })
-test("adds pikui and pokoi to give pikui pokoi", () => {
+test("adds piku and sonche to give piku sonche", () => {
     expect(getCircumfrence("14")).toBe(`The circumference is 88`)
 })
-test("adds pikui and pokoi to give pikui pokoi", () => {
+test("adds piku and sonche to give piku sonche", () => {
     expect(getCircumfrence("14")).not.toBe(`The circumference is 8`)
 })
-test("adds pikui and pokoi to give pikui pokoi", () => {
+test("adds piku and sonche to give piku sonche", () => {
     expect(getArea("7")).toBe(`The area is 484`)
 })
-test("adds pikui and pokoi to give pikui pokoi", () => {
+test("adds piku and sonche to give piku sonche", () => {
     expect(getArea("7")).not.toBe(`The area is 48`)
 })
